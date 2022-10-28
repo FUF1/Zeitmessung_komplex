0d6eec6f9a47ae7a10080170b5d3bcb295d74b71
FUF1 <69962736+FUF1@users.noreply.github.com>
2022-10-29 01:16:46 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
+datalogger.onLogFull(function () {
+    music.playMelody("C5 A B G A F G E ", 200)
+    basic.showString("FEHLER: Log-Datei voll!")
+})
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.clearScreen()
+    start = 0
+    ende = 0
+    music.playMelody("E G E A G - - - ", 300)
+})
+let zeit = 0
+let ende = 0
+let start = 0
+start = 0
+ende = 0
+// Kugelschleuse schliessen.
+servos.P2.setAngle(180)
+pins.servoWritePin(AnalogPin.P3, 0)
+serial.redirectToUSB()
+serial.setBaudRate(BaudRate.BaudRate115200)
+while (!(input.logoIsPressed())) {
+    basic.pause(1)
+}
+// Wert 0 bedeutet, die Kugelschleuse ist offen.
+servos.P2.setAngle(0)
 basic.forever(function () {
-	
+    while (input.pinIsPressed(TouchPin.P0)) {
+        start = input.runningTime()
+        music.playTone(262, music.beat(BeatFraction.Whole))
+    }
+    while (input.pinIsPressed(TouchPin.P1)) {
+        ende = input.runningTime()
+        music.playTone(698, music.beat(BeatFraction.Whole))
+        basic.pause(100)
+        zeit = (ende - start) / 1000
+        datalogger.log(datalogger.createCV("Dauer", zeit))
+        datalogger.mirrorToSerial(true)
+        basic.showNumber(zeit)
+        basic.pause(200)
+        // Servo hebt Kreisscheibe an, um Neigung der Bahn umzukehren.
+        pins.servoWritePin(AnalogPin.P3, 180)
+        // Pausieren, bis oberer Pin geschlossen wird.
+        while (!(input.pinIsPressed(TouchPin.P0))) {
+            basic.pause(1)
+        }
+        basic.pause(500)
+        // Kugelschleuse schliessen.
+        servos.P2.setAngle(180)
+        // Bahn wieder absenken.
+        pins.servoWritePin(AnalogPin.P3, 0)
+        basic.pause(1000)
+        // Kugelschleuse öffnen, um Zyklus neu zu starten.
+        servos.P2.setAngle(0)
+        start = 0
+        ende = 0
+    }
 })
