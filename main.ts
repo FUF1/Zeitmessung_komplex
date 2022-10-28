c68cf5a45476e12aad7e33bfc13385c3c514cfb5
FUF1 <69962736+FUF1@users.noreply.github.com>
2022-10-29 01:58:35 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,10 @@ input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
     basic.clearScreen()
     start = 0
     ende = 0
+    // Bahn wieder absenken.
+    pins.servoWritePin(AnalogPin.P15, 180)
+    // Kugelschleuse schliessen.
+    servos.P2.setAngle(180)
     music.playMelody("E G E A G - - - ", 300)
 })
 let zeit = 0
@@ -15,9 +19,8 @@ start = 0
 ende = 0
 // Kugelschleuse schliessen.
 servos.P2.setAngle(180)
-pins.servoWritePin(AnalogPin.P3, 0)
-serial.redirectToUSB()
-serial.setBaudRate(BaudRate.BaudRate115200)
+pins.servoWritePin(AnalogPin.P15, 180)
+datalogger.setColumnTitles("Dauer")
 while (!(input.logoIsPressed())) {
     basic.pause(1)
 }
@@ -38,7 +41,7 @@ basic.forever(function () {
         basic.showNumber(zeit)
         basic.pause(200)
         // Servo hebt Kreisscheibe an, um Neigung der Bahn umzukehren.
-        pins.servoWritePin(AnalogPin.P3, 180)
+        pins.servoWritePin(AnalogPin.P15, 0)
         // Pausieren, bis oberer Pin geschlossen wird.
         while (!(input.pinIsPressed(TouchPin.P0))) {
             basic.pause(1)
@@ -46,9 +49,10 @@ basic.forever(function () {
         basic.pause(500)
         // Kugelschleuse schliessen.
         servos.P2.setAngle(180)
+        basic.pause(500)
         // Bahn wieder absenken.
-        pins.servoWritePin(AnalogPin.P3, 0)
-        basic.pause(1000)
+        pins.servoWritePin(AnalogPin.P15, 180)
+        basic.pause(2000)
         // Kugelschleuse öffnen, um Zyklus neu zu starten.
         servos.P2.setAngle(0)
         start = 0
